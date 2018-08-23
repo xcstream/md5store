081ca66c0139838e8b7548e55561a6e019ea6f8d
xcstream <cheng.xu@all-dream.com>
2018-08-23 17:03:34 +0800

    mqtt service

--- mqttservice.py
+++ mqttservice.py
@@ -1,5 +1,9 @@
-import paho.mqtt.client as mqtt
+# -*- coding: utf-8 -*-
 
+import paho.mqtt.client as mqtt
+import redis
+import json
+r = redis.Redis(host='localhost',port=8888,db=0)
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -8,6 +12,21 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     print(msg.topic + " " + ":" + str(msg.payload))
+    try:
+        data = json.loads(msg.payload)
+        inputmd5 = data["md5"]
+        clientId = data["clientId"]
+        result = r.get(inputmd5)
+        if result == None:
+            result = '未找到'
+        resultjson = json.dumps({'text':result})
+        client.publish(clientId, resultjson)
+
+    except ValueError:
+        return False
+    return True
+
+
 
 
 client = mqtt.Client()
