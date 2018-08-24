31ab85d39eef67dc452e963eeef764222436e6e1
xcstream <cheng.xu@all-dream.com>
2018-08-24 09:55:55 +0800

    set start end

---
+++ task.py
@@ -0,0 +1,20 @@
+import redis
+r = redis.Redis(host='localhost',port=8888,db=0)
+
+oldstart = r.get('m_start')
+oldend = r.get('m_end')
+
+print "start", oldend , 'end',oldend
+
+start = input("start:")
+end = input("end:")
+if start != '':
+    r.set('m_start',start)
+
+if end != '':
+    r.set('m_end',end)
+
+oldstart = r.get('m_start')
+oldend = r.get('m_end')
+print "start", oldend, 'end', oldend
+
